e628045c75931df15d38add5fa688f6e8b9949ce
Trevor Armes <trevor@Trevors-MacBook-Pro.local>
2022-04-19 15:47:18 -0400

    test

--- src/Components/Serverless.js
+++ src/Components/Serverless.js
@@ -5,13 +5,13 @@ import Backendless from 'backendless';
 function Serverless() {
 
     const getInst = () => {
-        
+        return Backendless.APIServices.invoke('CodelessShoppingCartService', 'getInstructions', null)
     }
 
     return (
         <div className='serverless-container'>
             <h5>Serverless</h5>
-
+            <button onClick={getInst}>Press</button>
         </div>
     )
 };
